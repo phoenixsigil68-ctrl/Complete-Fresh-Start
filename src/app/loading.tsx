f7303dc6ce914f95f458fea44dde3ebe24788d02
Phoenix <phoenixsigil68@gmail.com>
2025-12-06 15:04:04 +0530

    Loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import Lottie from "lottie-react";
+import Loader from "@/lib/loading.json";
+
+const loading = () => {
+  return (
+    <div className="w-full h-dvh flex justify-start max-sm:start max-lg:justify-start max-md:justify-start items-center">
+      <Lottie animationData={Loader} className="h-[200px]" loop={true}></Lottie>
+    </div>
+  );
+};
+
+export default loading;
